64d7556c8bf757952b84b0584ed290e146e39ba5
Zhida Liu <liuzhida2020@163.com>
2022-04-27 11:19:55 +0800

    第一次上传

---
+++ Assets/Script/MyDefaultTrackableEventHandler.cs
@@ -0,0 +1,116 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class MyDefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    public GameObject AiXiPrefab;
+    public GameObject bloodPrefab;
+    public GameObject tonadoPrefab;
+
+    #region PROTECTED_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        
+        //生成人物
+        GameObject AiXi = GameObject.Instantiate(AiXiPrefab, transform.position - new Vector3(0,3,0), transform.rotation);
+       // AiXi.transform.position = this.transform.position;
+        AiXi.transform.parent = this.transform;
+
+        //生成特效
+        GameObject effect1 = GameObject.Instantiate(bloodPrefab, transform.position, transform.rotation);
+        effect1.transform.parent = this.transform;
+        Destroy(effect1, 6.0f);
+
+        GameObject effect2 = GameObject.Instantiate(tonadoPrefab, transform.position, Quaternion.identity);
+        effect2.transform.parent = this.transform;
+        Destroy(effect2, 6.0f);
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        Destroy(GameObject.Find("AiXi(Clone)"));
+
+        Destroy(GameObject.Find("42_RFX_Blood_Puddle(Clone)"));
+        Destroy(GameObject.Find("48_RFX_Tonado_Flame(Clone)"));
+    }
+
+    #endregion // PROTECTED_METHODS
+}
